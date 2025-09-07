78cfc60dac8d2172db6e0127bf7cc661f3b87eba
differrari <git@diferrari.com>
2025-09-07 00:00:00 +0000

    Added colors with evolution

--- gol.c
+++ gol.c
@@ -10,46 +10,61 @@
 
 typedef union { 
     struct {
-        uint8_t rsvd: 5;
-        uint8_t flip_on: 1;
-        uint8_t flip_off: 1;
-        uint8_t alive: 1;
+        uint32_t color: 24;
+        uint32_t rsvd: 5;
+        uint32_t flip_on: 1;
+        uint32_t flip_off: 1;
+        uint32_t alive: 1;
     };
-    uint8_t cell;
+    uint32_t cell;
 } cell;
 
 cell *grid;
 int grid_x, grid_y;
 bool paused = false; 
+rng_t rng = {};
 
 #define SCALE 10
 #define BG_COLOR 0x0
-#define CELL_COLOR 0xFFb4dd13
+
+#define CELL_AT(x,y) grid[(y * grid_x) + x]
 
 void check_neighbors(){
     for (int x = 0; x < grid_x; x++){
         for (int y = 0; y < grid_y; y++){
             int neighbors = 0;
+            uint16_t r = rng_next8(&rng)/3;
+            uint16_t g = rng_next8(&rng)/3;
+            uint16_t b = rng_next8(&rng)/3;
             for (int nx = -1; nx <= 1; nx++){
                 for (int ny = -1; ny <= 1; ny++){
                     if ((nx + x) < 0 || (ny + y) < 0 || (nx + x) > grid_x || (ny + y) > grid_y || (ny == 0 && nx == 0)) continue;
-                    if (grid[((y + ny) * grid_x) + (x + nx)].alive) neighbors++;
+                    if (grid[((y + ny) * grid_x) + (x + nx)].alive){
+                        neighbors++;
+                        r += (grid[((y + ny) * grid_x) + (x + nx)].color >> 16) & 0xFF;
+                        g += (grid[((y + ny) * grid_x) + (x + nx)].color >> 8) & 0xFF;
+                        b += (grid[((y + ny) * grid_x) + (x + nx)].color >> 0) & 0xFF;
+                    }
                 }
             }
-            if (grid[(y * grid_x) + x].alive && (neighbors < 2 || neighbors > 3)){
-                // printf("DEAD %ix%i", x, y);
-                grid[(y * grid_x) + x].flip_off = 1;
+            r /= neighbors;
+            g /= neighbors;
+            b /= neighbors;
+
+            r &= 0xFF;
+            g &= 0xFF;
+            b &= 0xFF;
+
+            if (CELL_AT(x, y).alive && (neighbors < 2 || neighbors > 3)){
+                CELL_AT(x, y).flip_off = 1;
             }
-            else if (!grid[(y * grid_x) + x].alive && neighbors == 3){
-                // printf("ALIVE %ix%i", x, y);
-                grid[(y * grid_x) + x].flip_on = 1;
+            else if (!CELL_AT(x, y).alive && neighbors == 3){
+                CELL_AT(x, y).color = (r << 16) | (g << 8) | (b << 0);
+                CELL_AT(x, y).flip_on = 1;
             }
         }
     }
-    
 }
-
-#define CELL_AT(x,y) grid[(y * grid_x) + x]
 
 int main(int argc, char* argv[]){
     draw_ctx ctx = {};
@@ -57,12 +72,13 @@ int main(int argc, char* argv[]){
     grid_x = (int)min(100, max(10, ctx.width/SCALE));
     grid_y = (int)min(100, max(10, ctx.height/SCALE));
     grid = (cell*)malloc(grid_x*grid_y*sizeof(cell));
-    rng_t rng = {};
     rng_seed(&rng, get_time());
     for (uint32_t x = 0; x < grid_x; x++)
         for (uint32_t y = 0; y < grid_y; y++){
-            grid[(y * grid_x) + x].flip_on = rng_next8(&rng) < 30;
-            grid[(y * grid_x) + x].alive = grid[(y * grid_x) + x].flip_on; 
+            if (rng_next8(&rng) < 30){
+                CELL_AT(x, y).flip_on = 1;
+                CELL_AT(x, y).color = (x << 16) | (y << 8) | min(rng_next8(&rng), 100) ;
+            }
         }
     
     fb_clear(&ctx, BG_COLOR);
@@ -74,20 +90,22 @@ int main(int argc, char* argv[]){
         if (read_key(&kp)){
             if (kp.keys[0] == KEY_SPACE) paused = !paused;
             if (kp.keys[0] == KEY_ENTER || kp.keys[0] == KEY_KPENTER) step = true;
+            if (kp.keys[0] == KEY_ESC) halt(0);
         }
         if (!paused || step){
             ctx.full_redraw = true;
             for (int x = 0; x < grid_x; x++){
                 for (int y = 0; y < grid_y; y++){
-                    if (grid[(y * grid_x) + x].flip_on){
-                        fb_fill_rect(&ctx, x * SCALE, y * SCALE, SCALE, SCALE, CELL_COLOR);
-                        grid[(y * grid_x) + x].flip_on = 0;
-                        grid[(y * grid_x) + x].alive = 1;
+                    cell cell = CELL_AT(x, y);
+                    if (cell.flip_on){
+                        fb_fill_rect(&ctx, x * SCALE, y * SCALE, SCALE, SCALE, (0xFF << 24) | cell.color);
+                        CELL_AT(x, y).flip_on = 0;
+                        CELL_AT(x, y).alive = 1;
                     }
-                    if (grid[(y * grid_x) + x].flip_off){
+                    if (cell.flip_off){
                         fb_fill_rect(&ctx, x * SCALE, y * SCALE, SCALE, SCALE, BG_COLOR);
-                        grid[(y * grid_x) + x].flip_off = 0;
-                        grid[(y * grid_x) + x].alive = 0;
+                        CELL_AT(x, y).flip_off = 0;
+                        CELL_AT(x, y).alive = 0;
                     }
                 }
             }
